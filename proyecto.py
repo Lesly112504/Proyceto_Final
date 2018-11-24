db0aef680097482bd6dd4922fd038be5f70df234
Lesly112504 <43552303+Lesly112504@users.noreply.github.com>
2018-11-24 13:13:07 -0600

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -15,13 +15,13 @@ sub3 = int(f" {g3 * 8}")
 sub4 = int(f" {g4 * 8}")
 sub5 = int(f" {g5 * 8}")
 
-total =(f"  $ {sub1 + sub2 + sub3 + sub4 + sub5}")
+total =(sub1 + sub2 + sub3 + sub4 + sub5)
 
 Venta_garrafones = [['$8.00', 'Subtotal', '$10.00', 'Subtotal', '$11.00', 'Subtotal', '$12.00', 'Subtotal', '$15.00', 'Subtotal'], [g1, sub1, g2, sub2, g3, sub3, g4, sub4, g5, sub5]] 
                                                                                                                                        
 print(tabulate(Venta_garrafones, headers='firstrow', tablefmt='fancy_grid'))
 
-print(f"Tu venta total es de: {total} ")
+print(f"Tu venta de este viaje fue de: $ {total}")
 
 
 print("SEGUNDO VIAJE: ")
@@ -38,13 +38,13 @@ subf3 = int(f" {f3 * 8}")
 subf4 = int(f" {f4 * 8}")
 subf5 = int(f" {f5 * 8}")
 
-total2 =(f" $ {subf1 + subf2 + subf3 + subf4 + subf5}")
+total2 =(subf1 + subf2 + subf3 + subf4 + subf5)
 
 Venta_garrafones2 = [['$8.00', 'Subtotal', '$10.00', 'Subtotal', '$11.00', 'Subtotal', '$12.00', 'Subtotal', '$15.00', 'Subtotal'], [f1, subf1, f2, subf2, f3, subf3, f4, subf4, f5, subf5]] 
                                                                                                                                        
 print(tabulate(Venta_garrafones2, headers='firstrow', tablefmt='fancy_grid'))
 
-print(f"Tu venta total es de: {total2} ")
+print(f"Tu venta de este viaje fue de:$ {total2} ")
 
 
 print("TERCER VIAJE: ")
@@ -61,13 +61,13 @@ subh3 = int(f" {h3 * 8}")
 subh4 = int(f" {h4 * 8}")
 subh5 = int(f" {h5 * 8}")
 
-total3 =(f" $ {subh1 + subh2 + subh3 + subh4 + subh5}")
+total3 =(subh1 + subh2 + subh3 + subh4 + subh5)
 
 Venta_garrafones3 = [['$8.00', 'Subtotal', '$10.00', 'Subtotal', '$11.00', 'Subtotal', '$12.00', 'Subtotal', '$15.00', 'Subtotal'], [h1, subh1, h2, subh2, h3, subh3, h4, subh4, h5, subh5]] 
                                                                                                                                        
 print(tabulate(Venta_garrafones3, headers='firstrow', tablefmt='fancy_grid'))
 
-print(f"Tu venta total es de: {total3} ")
+print(f"Tu venta de este viaje fue de: ${total3}")
 
 
 print("CUARTO VIAJE: ")
@@ -84,13 +84,13 @@ subi3 = int(f" {i3 * 8}")
 subi4 = int(f" {i4 * 8}")
 subi5 = int(f" {i5 * 8}")
 
-total4 =(f" $ {subi1 + subi2 + subi3 + subi4 + subi5}")
+total4 =(subi1 + subi2 + subi3 + subi4 + subi5)
 
 Venta_garrafones4 = [['$8.00', 'Subtotal', '$10.00', 'Subtotal', '$11.00', 'Subtotal', '$12.00', 'Subtotal', '$15.00', 'Subtotal'], [i1, subi1, i2, subi2, i3, subi3, i4, subi4, i5, subi5]] 
                                                                                                                                        
 print(tabulate(Venta_garrafones4, headers='firstrow', tablefmt='fancy_grid'))
 
-print(f"Tu venta total es de: {total4} ")
+print(f"Tu venta de este viaje fue de: ${total4}")
 
 
 print("QUINTO VIAJE: ")
@@ -107,12 +107,15 @@ subj3 = int(f" {j3 * 8}")
 subj4 = int(f" {j4 * 8}")
 subj5 = int(f" {j5 * 8}")
 
-total5 =(f" $ {subj1 + subj2 + subj3 + subj4 + subj5}")
+total5 =(subj1 + subj2 + subj3 + subj4 + subj5)
 
 Venta_garrafones5 = [['$8.00', 'Subtotal', '$10.00', 'Subtotal', '$11.00', 'Subtotal', '$12.00', 'Subtotal', '$15.00', 'Subtotal'], [j1, subj1, j2, subj2, j3, subj3, j4, subj4, j5, subj5]] 
                                                                                                                                        
 print(tabulate(Venta_garrafones5, headers='firstrow', tablefmt='fancy_grid'))
 
-print(f"Tu venta total del dia es de: {total5} ")
+print(f"Tu venta de este vaije fue de: ${total5}")
 
-print(f" ${total} + ${total2} + ${total3} + ${total4} + ${total5} = ${total + total2 + total3 + total4 + total5}")
+total_dia = (f" Tu venta del dia es de: ${total + total2 + total3 + total4 + total5}")
+total_dia
+             
+      
